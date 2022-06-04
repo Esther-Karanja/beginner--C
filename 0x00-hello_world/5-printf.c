0c3cb2cf1dbe27dd341a9c4c52b8ed76188f571c
Esther-Karanja <est.karanja@gmail.com>
2022-06-04 04:25:38 -0700

    C-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Enty point
+ *
+ * Return: Always  0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

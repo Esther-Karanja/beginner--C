7938e7e068d130f018c1598a18425eeb8c8045ee
Esther-Karanja <est.karanja@gmail.com>
2022-06-09 13:34:31 -0700

    functions, nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul- multiplies two integers
+ * @a: input
+ * @b: input
+ *
+ * Return: always 0 (success)
+*/
+int mul(int a, int b)
+{
+	_putchar ('a');
+	_putchar ('*');
+	_putchar ('b');
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers 0-9 followed by a new line
+ *
+ * Return: always 0 (success)
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar (i);
+	}
+	_putchar ('\n');
+}

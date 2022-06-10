6e98bdede23f6afd1f55500555f49fd9a1cff045
Esther-Karanja <est.karanja@gmail.com>
2022-06-09 19:44:01 -0700

    functions, nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
 /**
  * more_numbers - prints the numbers 0-14 ten times
  *
- * Return: always 0 (success)
+ * Return: 0 (success)
 */
 void more_numbers(void)
 {
@@ -15,10 +15,10 @@ void more_numbers(void)
 		for (nums = 0; nums <= 14; nums++)
 
 			if (nums > 9)
+
 			{
 				_putchar ((nums / 10) + '0');
 			_putchar ((nums % 10) + '0');
-
 			}
 	_putchar ('\n');
 	}

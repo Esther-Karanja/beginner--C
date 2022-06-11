3437c6f1b9cc2ed03958e5a9efb86efca2283799
Esther-Karanja <est.karanja@gmail.com>
2022-06-11 04:43:34 -0700

    functions, nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @r: integer input
+ *
+ * Return: always 0 (success)
+*/
+int _abs(int r)
+{
+	if ((r > 0) || (-1(r < 0)))
+	{
+		_putchar (r);
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @i: number input
+ *
+ * Return: 0 (success)
+*/
+int print_last_digit(int i)
+{
+	_putchar (i % 10);
+	return (0);
+}

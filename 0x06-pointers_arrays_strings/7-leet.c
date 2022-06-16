b2eeab2126bca3381f6cc5d7161564c0527dfa5e
Esther-Karanja <est.karanja@gmail.com>
2022-06-16 09:45:15 -0700

    more arrays and pointers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ * @st: string to encode
+ *
+ * Return: pointer to new string
+*/
+char *leet(char *st)
+{
+	int i = 0;
+	char *ptr = st;
+
+	if (st[i] != '\0')
+	{
+		st[i] = st[i] - 32;
+	}
+	return (ptr);
+}

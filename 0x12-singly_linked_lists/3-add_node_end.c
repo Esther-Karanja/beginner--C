6a75f5b29016f159eb59611286520c824b564919
Esther-Karanja <est.karanja@gmail.com>
2022-07-14 14:06:44 -0700

    fix1

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,7 +15,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new;
 	list_t *temp;
 
-	new = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t *));
 	if (str == NULL)
 		return (NULL);
 	if (new == NULL)

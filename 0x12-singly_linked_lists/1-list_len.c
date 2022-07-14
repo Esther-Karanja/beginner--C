01557af14efa62578df24e25fdf171321a2e7df6
Esther-Karanja <est.karanja@gmail.com>
2022-07-14 07:19:01 -0700

    first commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * list_len -  function that returns the number of elements in a
+ * linked list_t list
+ * @h: pointer to the first elememt of list_t
+ *
+ * Return: number of elements
+*/
+size_t list_len(const list_t *h)
+{
+	unsigned int i;
+	const list_t *ptr;
+
+	ptr = h;
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+		i++;
+	}
+
+	return (i);
+}
